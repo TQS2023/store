e242654768288e12d7efb46fbdf5a8ab5b6b3686
LuisCarlos104171 <luiscarlos.c.afonso@ua.pt>
2023-06-22 22:43:47 +0100

    Added the skeleton for the user authentication service

--- src/main/java/pt/ua/deti/store/controllers/AuthenticationApi.java
+++ src/main/java/pt/ua/deti/store/controllers/AuthenticationApi.java
@@ -1,6 +1,5 @@
 package pt.ua.deti.store.controllers;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pt.ua.deti.store.entities.*;
---
+++ src/main/java/pt/ua/deti/store/services/AuthenticationService.java
@@ -0,0 +1,23 @@
+package pt.ua.deti.store.services;
+
+import org.springframework.stereotype.Service;
+import pt.ua.deti.store.entities.*;
+
+@Service
+public class AuthenticationService {
+    public TokenResponse login(LoginRequest loginRequest) {
+        return null;
+    }
+
+    public TokenResponse register(UserRequest user) {
+        return null;
+    }
+
+    public ProfileResponse profile(String user) {
+        return null;
+    }
+
+    public ProfileUpdateResponse updateProfile(String user, ProfileRequest profileUpdateRequest) {
+        return null;
+    }
+}
